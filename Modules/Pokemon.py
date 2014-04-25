9c629a274ba4f703726cc966702013ff0ce2a0a9
Akshay Dixit <akshaydixi@gmail.com>
2014-04-26 02:18:43 +0530

    Pokemon plugin for bot

---
+++ Modules/Pokemon.py
@@ -0,0 +1,43 @@
+import pykemon
+from random import choice
+
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+
+class PokemonBot():
+  def __init__(self):
+    pass
+
+  def image(self,param):
+    if is_number(param):
+      try:
+        url = pykemon.get(sprite=int(param)).image
+        return {'url' : 'http://pokeapi.co/' + url}
+      except:
+        return {'error' : '404 - Wrong pokemon id'}
+    else:
+      try:
+        pid = str(pykemon.get(pokemon=param).id)
+        return {'url' : 'http://pokeapi.co//media/img/' + pid + '.png'}
+      except:
+        return {'error' : '404 - Wrong pokemon name'}
+
+  def description(self,param):
+	try:
+		pokemon = pykemon.get(pokemon=param)
+		description = choice(pokemon.descriptions.values())[:-1]
+		num = description.split('/')[-1]
+		description_text = pykemon.get(description=num).description
+		return {'text' : description_text }
+	except e:
+		return {'error' : '404 - Wrong Pokemon query'}
+          
+        
+      
+
+
+  
